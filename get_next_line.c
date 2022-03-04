1ace29fac1b74d247a5a2b6b65c30a11ec6242fd
Jonathan Ting <87597182+jonting56@users.noreply.github.com>
2022-03-04 11:59:10 +1030

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,93 +6,91 @@
 /*   By: jting <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:20:53 by jting             #+#    #+#             */
-/*   Updated: 2022/02/25 16:33:30 by jting            ###   ########.fr       */
+/*   Updated: 2022/03/04 11:58:40 by jting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_utils.c"
+#include <stdio.h>
 #include "get_next_line.h"
 
-char    *ft_read_all(int fd, char *s_remain)
+char	*ft_read_all(int fd, char *s_remain)
 {
-    char *buff;
-    int read_bytes;
+	char	*buff;
+	int		read_bytes;
 
-    buff = malloc((*buff) * (BUFFER_SIZE + 1));
-    if (!buff)
-        return (NULL);
-    read_bytes = 1;
-    while(!ft_strchr(s_remain, '\n') && read_bytes != 0)
-    {
-        read_bytes = read(fd, buff, BUFFER_SIZE);
-        if (read_bytes = -1)
-        {
-            free(buff);
-            return (NULL);
-        }
-        buff[read_bytes] = '\0';
-        s_remain = strjoin(s_remain, buff);
-    }
-    free(buff);
-    return (s_remain);
+	buff = malloc(sizeof(*buff) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(s_remain, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buff, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[read_bytes] = '\0';
+		s_remain = ft_strjoin(s_remain, buff);
+	}
+	free(buff);
+	return (s_remain);
 }
 
-char    *ft_get_line(s_remain)
+char	*ft_get_line(char *s_remain)
 {
-    int i;
-    char    *str;
+	int		i;
+	char	*str;
 
-    i = 0;
-    while (s_remain[i] && s_remain != '\n')
-        i++;
-    str = malloc(sizeof(*str) * (i + 2));
-    if (!str)
-        return (NULL);
-    i = 0;
-    while (s_remain[i] && s_remain[i] != '\n')
-    {
-        str[i] = s_remain[i];
-        i++;
-    }
-    str[i] = '\0';
-    return (str);
+	i = 0;
+	while (s_remain[i] && s_remain[i] != '\n')
+		i++;
+	str = malloc(sizeof(*str) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s_remain[i] && s_remain[i] != '\n')
+	{
+		str[i] = s_remain[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
-char    *ft_remainder(char *s_remain)
+char	*ft_remainder(char *s_remain)
 {
-    int i;
-    int j;
-    char    *str;
+	int		i;
+	int		j;
+	char	*str;
 
-    i = 0;
-    while (s_remain[i] && s_remain =! '\n')
-        i++;
-    str = malloc(sizeof(*str) * (ft_strlen(s_remain) - i + 1))
-    if (!str)
-    {
-        free(str);
-        return (NULL);
-    }
-    j = -1;
-    while (s_remain[i])
-        str[++j] = s_remain[i++]
-    str[j] = '\0';
-    free(s_remain);
-    return (str);
+	i = 0;
+	while (s_remain[i] && s_remain[i] != '\n')
+		i++;
+	str = malloc(sizeof(*str) * (ft_strlen(s_remain) - i + 1));
+	if (!str)
+	{
+		free(str);
+		return (NULL);
+	}
+	j = -1;
+	while (s_remain[i++])
+		str[++j] = s_remain[i];
+	str[j] = '\0';
+	return (str);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char        *line;
-    static char *s_remain;
+	char		*line;
+	static char	*s_remain;
 
-    if (fd < 0 || BUFFER_SIZE < 1)
-        return (0);
-    s_remain = ft_read_all(fd, s_remain);
-    if (!s_remain)
-        return (NULL);
-    line = ft_get_line(s_remain);
-    s_remain = ft_remainder(s_remain);
-    return (line);
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (0);
+	s_remain = ft_read_all(fd, s_remain);
+	if (!s_remain)
+		return (NULL);
+	line = ft_get_line(s_remain);
+	s_remain = ft_remainder(s_remain);
+	return (line);
 }
-
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,56 +6,55 @@
 /*   By: jting <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 12:39:53 by jting             #+#    #+#             */
-/*   Updated: 2022/02/25 12:54:50 by jting            ###   ########.fr       */
+/*   Updated: 2022/03/04 11:58:30 by jting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-        i++;
-}   return (i);
-
-char    *ft_strjoin(char *s1, char *s2)
-{
-    int i;
-    int j;
-    char    *dst;
-
-    if(!s1)
-    {
-        s1 = malloc(sizeof(*s1) * 1);
-        s1[0] = '\0';
-    }
-    dst = malloc(sizeof(*dst) * (ft_strlen(s1) + ft_strlen(s2) + 1)
-    if (!dst)
-        return (NULL);
-    i = -1;
-    while(s1[++i])
-        dst[i] = s1[i];
-    j = -1;
-    while (s2[++j])
-        dst[i + j] = s2[j]
-    dst[i + j] = '\0';
-    free(s2);
-    return (dst);
+	i = 0;
+	while (str[i])
+	i++;
+	return (i);
 }
 
-char    *ft_strchr(char *s, int c)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    int i;
+	int		i;
+	int		j;
+	char	*dst;
 
-    if (!s || c == '\0')
-        return (NULL);
-    i = 0;
-    while (s[i] && s[i] != c)
-        i++;
-    if (s[i] == c)
-        return ((char *)(s + i));
-    return (0);
+	
+	dst = malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dst)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		dst[i] = s1[i];
+	j = 0;
+	while (s2[j++])
+		dst[i + j] = s2[j];
+	dst[i + j] = '\0';
+	free(s2);
+	return (dst);
+}
+
+
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	if (!s || c == '\0')
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	if (s[i] == c)
+		return ((char *)(s + i));
+	return (0);
 }
